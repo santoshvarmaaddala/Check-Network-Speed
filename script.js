996538a960dd69c2906630fb383977327d4b44f9
Santosh Varma Addala <santoshvarma2166@gmail.com>
2024-09-23 21:00:13 +0530

    theme and structure done

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// for start the meter
+document.getElementById("start-btn").onclick = function(){
+    document.getElementById("download").innerText = "Down";
+}
+
+// to refresh the page
+document.getElementById("refresh-btn").onclick = function(){
+    location.reload();
+}
