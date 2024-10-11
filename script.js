d87784e97ddde9a240f9b248030acd52838d86df
Santosh Varma Addala <santoshvarma2166@gmail.com>
2024-10-11 11:08:51 +0530

    desktop done

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-// to refresh the page
 document.getElementById("refresh-btn").onclick = function(){
     location.reload();
 }
 
 document.getElementById('start-btn').onclick = () => {
-    document.getElementById('download-speed').textContent = "Testing...";
-    document.getElementById('upload-speed').textContent = "Testing...";
     document.getElementById('ping-value').textContent = "Testing...";
-
-    startSpeedTest();
+    
+    const myTimeout = setTimeout(startSpeedTest, 1000);
 }
 
+function errorMsg(){
+    alert("Error while Testing");
+    console.log("Testing error");
+}
 
+function getSummary(ping){
+    summaryList = ["Your Network Speed is Weak", "Your Network Speed is Good", "Your Network Speed is High"];
+    if(ping <= 500){
+        document.getElementById("summary").textContent = summaryList[0];
+    }else if(ping > 500 && ping <= 1000){
+        document.getElementById("summary").textContent = summaryList[1];
+    }else{
+        document.getElementById("summary").textContent = summaryList[2];
+    }
+}
 
 function startSpeedTest(){
     const startTime = Date.now();
@@ -21,49 +32,12 @@ function startSpeedTest(){
 
     pingImg.onload = () =>{
         const ping = Date.now() - startTime;
-        document.getElementById('ping-value').textContent = ping + " ms";
-
-        calculateDownloadSpeed();
+        document.getElementById('ping-value').textContent = ping + " mbps";
+        getSummary(ping);
     }
     pingImg.onerror = () => {
         document.getElementById("ping-value").textContent = "Error";
+        errorMsg();
     }
 }
 
-function calculateDownloadSpeed(){
-    const downloadsize = 5 * 1024 * 1024;
-    const startTime = Date.now();
-
-    const downloadImg = new Image();
-    downloadImg.src = "https://fast.com/app-ed402d.js";
-
-    downloadImg.onload = () => {
-        const endTime = Date.now();
-        const duration = (endTime - startTime) / 1000;
-        const speedBps = (downloadsize * 8) / duration;
-        const speedMbps = (speedBps / (1024 * 1024)).toFixed(2);
-
-        document.getElementById("download-speed").textContent = speedMbps + " Mbps";
-
-        calculateUploadSpeed();
-    }
-
-    downloadImg.onerror = () => {
-        document.getElementById('download-speed').textContent = "Error";
-    }
-}
-
-function calculateUploadSpeed() {
-
-    const uploadSize = 2 * 1024 * 1024; 
-    const startTime = Date.now();
-
-    setTimeout(() => {
-        const endTime = Date.now();
-        const duration = (endTime - startTime) / 1000; 
-        const speedBps = (uploadSize * 8) / duration; 
-        const speedMbps = (speedBps / (1024 * 1024)).toFixed(2); 
-
-        document.getElementById("upload-speed").textContent = speedMbps + " Mbps";
-    }, 2000); 
-}
